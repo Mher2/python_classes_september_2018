77322e16132d7e20a9a807085ea35b6a75d7ec3e
Mher Simonyan <mher.simonyan@c-labs.ch>
2018-09-11 11:20:49 +0400

    Updated and optimized prime numbers algorithm

--- Lesson1/Homework_prime_numbers.py
+++ Lesson1/Homework_prime_numbers.py
@@ -1,24 +1,34 @@
-numbers_list = [18, 1, 2, 5, 7, 17, -8, 12, 13]
+numbers_list = [181, 1, 259, 5, 7, 170, -8, 120, 131]
 prime_numbers_list = []
 
 for number in numbers_list:
+    print("Checking item: " + str(number))
     is_prime_number = True
+    divisible_list = []
+
     if number > 2:
-
-        # print(int(number ** 0.5) + 1)
-        # for i in range(2, int(number ** 0.5) + 1):    Approach 2 is Faster
-
-        for i in range(2, number):  # Approach 1
-            print(str(number) + " % " + str(i) + " = " + str(number % i))
-
+        for i in range(2, int(number ** 0.5) + 1):   # Approach 3 is Much more faster
+            is_divisible = True
+            if i not in divisible_list:
+                for j in divisible_list:
+                    if i % j == 0:
+                        is_divisible = False
+                        break
+                if is_divisible:
+                    divisible_list.append(i)
+                    print(str(number) + " % " + str(i) + " = " + str(number % i))
             if number % i == 0:
                 is_prime_number = False
                 break
 
+    print("divisibles: " + str(divisible_list))
     if is_prime_number and number > 1:
         print("Item " + str(number) + " is prime.")
         prime_numbers_list.append(number)
-        print("---")
+
+    else:
+        print("Item " + str(number) + " is not prime.")
+    print("---")
 
 print("Numbers list is: " + str(numbers_list))
 print("Prime numbers list is: " + str(prime_numbers_list))
